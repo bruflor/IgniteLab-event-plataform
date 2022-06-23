e98476fbf2555416652a108e244d2c6bf18f1fd6
Bruna Flôr <brn.flor@gmail.com>
2022-06-23 21:58:04 +0100

    Router and Video component

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,10 +1,20 @@
 import { gql, useQuery } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { client } from "./lib/apollo";
 import { Event } from "./pages/Event";
+
+import { Router } from "./Router";
 
 function App() {
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        {/* <Event /> */}
+      </ApolloProvider>
     </div>
   );
 }
---
+++ event-platform/src/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Event } from "./pages/Event";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>}></Route>
+      <Route path="/event" element={<Event />}></Route>
+      {/* :parametro */}
+      <Route path="/event/lesson/:slug" element={<Event />}></Route>
+    </Routes>
+  );
+}
